d316b62175a151510822c64e94ca8d654a1d2003
bakatsuyuki <bakatsuyuki@icloud.com>
2022-04-09 09:33:58 +0900

    Add routes const

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'const/routes.dart';
 import 'pages/counter_page.dart';
 
 final GoRouter router = GoRouter(
   routes: <GoRoute>[
     GoRoute(
-      path: '/',
+      path: Routes.root,
       builder: (BuildContext context, GoRouterState state) => const CounterPage(
         title: 'Flutter Demo Home Page',
       ),
