0136f0bc4b358e9f2501704b946171d131473dda
bakatsuyuki <bakatsuyuki@icloud.com>
2022-04-09 09:27:58 +0900

    Setup go_router

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'pages/counter_page.dart';
 
@@ -8,14 +9,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ProviderScope(
-        child: CounterPage(title: 'Flutter Demo Home Page'),
+    return ProviderScope(
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
       ),
     );
   }
 }
+
+final GoRouter _router = GoRouter(
+  routes: <GoRoute>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) => const CounterPage(
+        title: 'Flutter Demo Home Page',
+      ),
+    ),
+  ],
+);
