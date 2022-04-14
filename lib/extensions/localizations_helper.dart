ff7fe0c506e2ad3e589a181c1adc60a70b7208ad
bakatsuyuki <bakatsuyuki@icloud.com>
2022-04-15 00:03:17 +0900

    Ignore unnecessary warning

--- lib/extensions/localizations_helper.dart
+++ lib/extensions/localizations_helper.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: depend_on_referenced_packages
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension LocalizationsHelper on BuildContext {
