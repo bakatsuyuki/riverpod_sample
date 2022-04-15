ae752083f664397ea3c624e08f7f6ee6a3c4e86c
bakatsuyuki <bakatsuyuki@icloud.com>
2022-04-16 08:20:39 +0900

    CounterPageBody

--- lib/pages/counter_page/counter_page_body.dart
+++ lib/pages/counter_page/counter_page_body.dart
@@ -8,19 +8,16 @@ class CounterPageBody extends ConsumerWidget {
   const CounterPageBody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context,WidgetRef ref)  => Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          context.localizations.pushedTimesAre,
+  Widget build(BuildContext context, WidgetRef ref) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(context.localizations.pushedTimesAre),
+            Text(
+              '${ref.watch(counter)}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
-        Text(
-          '${ref.watch(counter)}',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-      ],
-    ),
-  );
+      );
 }
-
