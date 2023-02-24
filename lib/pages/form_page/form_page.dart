9a64ce2e0a593e49c43b7ad13b8fe067b3ed75ad
bakatsuyuki <bakatsuyuki@icloud.com>
2023-02-24 23:56:21 +0900

    fix isEnabledProvider

--- lib/pages/form_page/form_page.dart
+++ lib/pages/form_page/form_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_sample/extensions/localizations_helper.dart';
 
 class FormPage extends StatelessWidget {
@@ -28,8 +29,10 @@ class _Body extends ConsumerWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: ListView(
-          children:
-              _FormValueType.values.map((e) => _TextField(type: e)).toList(),
+          children: [
+            ..._FormValueType.values.map((e) => _TextField(type: e)),
+            const _SubmitButton(),
+          ],
         ),
       ),
     );
@@ -53,11 +56,29 @@ class _TextField extends ConsumerWidget {
       );
 }
 
+class _SubmitButton extends ConsumerWidget {
+  const _SubmitButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TextButton(
+      onPressed: ref.watch(isEnabledProvider) ? () {} : null,
+      child: Text(context.localizations.submit),
+    );
+  }
+}
+
 final textEditingControllerProviders = ChangeNotifierProvider.family(
   (ref, _FormValueType _) => TextEditingController(),
 );
 
-final isEnabledProvider = Provider((_) => false);
+final isEnabledProvider = Provider(
+  (ref) => _FormValueType.values
+      .map((e) => ref.watch(textEditingControllerProviders(e)))
+      .none(
+        (element) => element.text.isEmpty,
+      ),
+);
 
 enum _FormValueType {
   id,
