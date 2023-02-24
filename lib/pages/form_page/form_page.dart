9706fc7bab1d09411ced4d82545d0c928ec6962c
bakatsuyuki <bakatsuyuki@icloud.com>
2023-02-24 22:29:38 +0900

    Refactor the code

--- lib/pages/form_page/form_page.dart
+++ lib/pages/form_page/form_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_sample/extensions/localizations_helper.dart';
-
-import 'form_page_body.dart';
 
 class FormPage extends StatelessWidget {
   const FormPage({Key? key}) : super(key: key);
@@ -9,7 +8,7 @@ class FormPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const FormPageBody(),
+      body: const _Body(),
       appBar: AppBar(
         title: Text(
           context.localizations.form,
@@ -18,3 +17,123 @@ class FormPage extends StatelessWidget {
     );
   }
 }
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: ListView(
+            children: const [
+              _IdTextField(),
+              _PasswordTextField(),
+              _NameTextField(),
+              _PostalCodeTextField(),
+              _AddressTextField(),
+              _HobbyTextField(),
+              _OneLastWordTextField(),
+            ],
+          ),
+        ),
+      );
+}
+
+class _IdTextField extends StatelessWidget {
+  const _IdTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(labelText: context.localizations.id),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _PasswordTextField extends StatelessWidget {
+  const _PasswordTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(labelText: context.localizations.password),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _NameTextField extends StatelessWidget {
+  const _NameTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(labelText: context.localizations.name),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _PostalCodeTextField extends StatelessWidget {
+  const _PostalCodeTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration:
+            InputDecoration(labelText: context.localizations.postalCode),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _AddressTextField extends StatelessWidget {
+  const _AddressTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(
+          labelText: context.localizations.address,
+        ),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _HobbyTextField extends StatelessWidget {
+  const _HobbyTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(labelText: context.localizations.hobby),
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+class _OneLastWordTextField extends StatelessWidget {
+  const _OneLastWordTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(
+          labelText: context.localizations.oneLastWord,
+        ),
+        textInputAction: TextInputAction.done,
+        onFieldSubmitted: (value) {
+          //print(value);
+        },
+      );
+}
+
+final isEnabledProvider = Provider((_) => false);
