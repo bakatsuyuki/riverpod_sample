c6fdcbd92c59be7e3e87c5b3a189d7794c5a8e11
bakatsuyuki <bakatsuyuki@icloud.com>
2023-02-24 22:07:34 +0900

    Remove theme from test app.

--- test/helper.dart
+++ test/helper.dart
@@ -9,9 +9,6 @@ class TestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
